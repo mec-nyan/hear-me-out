1e404038ba7536b8a697807f3754c31ae67f678f
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-05-06 19:52:51 -0300

    Basic start page and styles.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <h1>Hear me out1</h1>
-      <h2>Ear training for everyone</h2>
+    <div className='app'>
+      <div className='title'>
+        <small className='author'>Nano's</small>
+
+        {/* I've not decided the name yet. */}
+        <h1>Hear me out!</h1>
+
+        <h2>Ear training for everyone.</h2>
+      </div>
+
+      {/* We'll extract components later. */}
+      <button id='start'>Start</button>
+
+      <footer>Made with 💖 in <span className='helix'>Helix</span></footer>
     </div>
   )
 }
